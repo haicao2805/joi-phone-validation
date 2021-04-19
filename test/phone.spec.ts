1ded3388474e04ddc1363f84fad8225ac56f6b8c
Cao Chi Hai <haicao2805@gmail.com>
2021-04-20 01:09:06 +0700

    Add .prettierrc

--- test/phone.spec.ts
+++ test/phone.spec.ts
@@ -3,23 +3,23 @@ import { ListCountry } from "../phone/listCountry";
 import { JoiPhoneFormat } from "../phone/phone";
 
 describe("JoiPhoneFormat", () => {
-    const schemaDefault = (country: string, input: any) =>
-        Joi.object({
-            phone: JoiPhoneFormat.string().defaultPhoneFormat(country),
-        }).validate(input);
+      const schemaDefault = (country: string, input: any) =>
+            Joi.object({
+                  phone: JoiPhoneFormat.string().defaultPhoneFormat(country),
+            }).validate(input);
 
-    const schemaInternational = (country: string, input: any) =>
-        Joi.object({
-            phone: JoiPhoneFormat.string().internationalPhoneFormat(country),
-        }).validate(input);
+      const schemaInternational = (country: string, input: any) =>
+            Joi.object({
+                  phone: JoiPhoneFormat.string().internationalPhoneFormat(country),
+            }).validate(input);
 
-    it("Pass a default Vietnam's phone number", () => {
-        const { error } = schemaDefault("Vietnam", { phone: "0862334006" });
-        expect(error).toBeUndefined();
-    });
+      it("Pass a default Vietnam's phone number", () => {
+            const { error } = schemaDefault("Vietnam", { phone: "0862334006" });
+            expect(error).toBeUndefined();
+      });
 
-    it("Pass a default Vietnam's international phone number", () => {
-        const { error } = schemaInternational("Vietnam", { phone: "+84862334006" });
-        expect(error).toBeUndefined();
-    });
+      it("Pass a default Vietnam's international phone number", () => {
+            const { error } = schemaInternational("Vietnam", { phone: "+84862334006" });
+            expect(error).toBeUndefined();
+      });
 });

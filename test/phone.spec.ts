ffde65464276a30abd5fdfb101cdac0105e43256
Cao Chi Hai <haicao2805@gmail.com>
2021-04-20 00:59:30 +0700

    Add code test

---
+++ test/phone.spec.ts
@@ -0,0 +1,20 @@
+import * as Joi from "joi";
+import { ListCountry } from "../phone/listCountry";
+import { JoiPhoneFormat } from "../phone/phone";
+
+describe("JoiPhoneFormat", () => {
+    const schemaDefault = (country: string, input: any) =>
+        Joi.object({
+            phone: JoiPhoneFormat.string().defaultPhoneFormat(country),
+        }).validate(input);
+
+    const schemaInternational = (country: string, input: any) =>
+        Joi.object({
+            phone: JoiPhoneFormat.string().internationalPhoneFormat(country),
+        }).validate(input);
+
+    it("Pass a default Vietnam's phone number", () => {
+        const { error } = schemaDefault("Vietnam", { phone: "0862334006" });
+        expect(error).toBeUndefined();
+    });
+});

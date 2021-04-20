3149de5cfe594f114eea9e0e1f1f582c310616f5
Cao Chi Hai <haicao2805@gmail.com>
2021-04-20 17:17:26 +0700

    Add new method that validate both format

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,7 @@ import { JoiPhoneFormat } from "./phone/phone";
 
 export { JoiPhoneFormat };
 export default JoiPhoneFormat;
+
+const hello = JoiPhoneFormat.string().defaultPhoneFormat("vi");
+const hello2 = JoiPhoneFormat.string().internationalPhoneFormat("vi");
+const hello3 = JoiPhoneFormat.string().bothPhoneFormat("vi");
--- src/phone/phone.ts
+++ src/phone/phone.ts
@@ -11,10 +11,16 @@ interface JoiStringExtend extends Joi.StringSchema {
       defaultPhoneFormat(countryName: CountryCode): this;
 
       /**
-       * @description Specifies the default phone number format in the country.
+       * @description Specifies the international phone number format in the country.
        * @param countryName The country name in ISO format. For example: Viet Nam(vn), Japan(jp), India(in), United States of America(us)
        */
       internationalPhoneFormat(countryName: CountryCode): this;
+
+      /**
+       * @description Specifies the default or international phone number format in the country.
+       * @param countryName The country name in ISO format. For example: Viet Nam(vn), Japan(jp), India(in), United States of America(us)
+       */
+      bothPhoneFormat(countryName: CountryCode): this;
 }
 
 interface JoiPhoneFormatExtend extends Joi.Root {
@@ -28,6 +34,7 @@ export const JoiPhoneFormat: JoiPhoneFormatExtend = Joi.extend((joi) => {
             messages: {
                   "phone.defaultPhoneFormat": "should be the same format as {#country}'s phone number",
                   "phone.internationalPhoneFormat": "should be the same format as {#country}'s international phone number format",
+                  "phone.bothPhoneFormat": "should be the {#country}'s phone number format",
             },
             rules: {
                   defaultPhoneFormat: {
@@ -80,6 +87,34 @@ export const JoiPhoneFormat: JoiPhoneFormatExtend = Joi.extend((joi) => {
                               if (!regex.test(value)) return helpers.error("phone.internationalPhoneFormat", args);
 
                               return value;
+                        },
+                  },
+                  bothPhoneFormat: {
+                        method(country: string) {
+                              return this.$_addRule({
+                                    name: "bothPhoneFormat",
+                                    args: { country },
+                              });
+                        },
+                        args: [
+                              {
+                                    name: "country",
+                                    assert: (value) => {
+                                          const res = ListCountry.has(value);
+                                          return res;
+                                    },
+                                    message: "must be an invalid country",
+                              },
+                        ],
+                        validate: (value: string, helpers: Joi.CustomHelpers, args): any => {
+                              const regexObject = ListCountry.get(args["country"]);
+                              if (!regexObject) return helpers.error("phone.bothPhoneFormat", args);
+                              const regexInternational = new RegExp(regexObject.international);
+                              const regexDefault = new RegExp(regexObject.default);
+
+                              if (regexInternational.test(value)) return value;
+                              else if (regexDefault.test(value)) return value.replace(value.charAt(0), regexObject.prefix);
+                              else return helpers.error("phone.bothPhoneFormat", args);
                         },
                   },
             },
